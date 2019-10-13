1abb29801c70656e2d824817fabc4f81307da2cc
tr33nt00n <zachariabaraka8@gmail.com>
2019-10-13 20:47:37 +0300

    Modified the layouts ui Changes to be committed: 	modified:   app.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/layouts/layout.ejs

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').parse()
-}
-
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
@@ -11,17 +7,9 @@ app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static('public'))
+app.use('/assets', express.static('assets'));
+
 app.use('/', indexRouter)
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { 
-    useNewUrlParser: true
-})
-
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.listen(process.env.PORT || 3000)
