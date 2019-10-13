3d1759a1921dad61140712103e22495320b5d9cc
tr33nt00n <zachariabaraka8@gmail.com>
2019-10-13 20:50:19 +0300

    Assets Added

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: false,
+    controlArrows: false,
+    slidesNavigation: true,
+    anchors: ['section_1', 'section_2', 'section_3', 'section_4']
+
+})
